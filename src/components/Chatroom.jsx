5edd4a0434005099450498cc8185708fccd47459
Abdallah <bratosab@gmail.com>
2024-07-24 09:27:52 +0200

    feat(Message): added message form and Message component

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import { Message } from "./Message";
+
 export function Chatroom() {
-    return (
-        <>
-        Chatroom
-        </>
-    )
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (formEvent) => {
+    formEvent.preventDefault();
+
+    console.log(message)
+  };
+
+  return (
+    <>
+      <main>
+        <Message />
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          placeholder="Say hi to the others..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,5 @@
+export function Message() {
+    return (
+        <>message</>
+    )
+}
