1dda8ce044443058d0a040c134897759c8c102c4
Abdallah <bratosab@gmail.com>
2024-07-24 10:18:46 +0200

    feat(chatroom): added connection to firebase

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 import { Message } from "./Message";
+import { addDoc, collection, orderBy, query } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { auth, store } from "../firebaseInstance";
 
 export function Chatroom() {
+  const messageCollection = collection(store, "messages");
+  const q = query(messageCollection, orderBy("timestamp"));
+
+  const [messages] = useCollectionData(q);
   const [message, setMessage] = useState("");
 
   const sendMessage = (formEvent) => {
     formEvent.preventDefault();
 
-    console.log(message)
+    const newMessage = {
+      message: message, 
+      timestamp: Date.now(),
+      userId: auth.currentUser.uid,
+      avatar: auth.currentUser.photoURL
+    }
+
+    addDoc(messageCollection, newMessage)
+    setMessage("")
   };
 
   return (
     <>
       <main>
-        <Message />
+        {messages &&
+          messages.map((msg) => (
+            <Message
+              key={msg.userId + msg.timestamp}
+              timestamp={msg.timestamp}
+              message={msg.message}
+              avatar={msg.avatar}
+              userId={msg.userId}
+            />
+          ))}
       </main>
       <form onSubmit={sendMessage}>
         <input
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,19 @@
-export function Message() {
-    return (
-        <>message</>
-    )
+
+import PropTypes from 'prop-types';
+
+export function Message({ avatar, message, timestamp, userId }) {
+  return (
+      <div>
+        <img src={avatar} alt="User avatar" />
+        <p>{message}</p>
+        <span>{timestamp}</span>
+      </div>
+  );
+}
+
+Message.propTypes = {
+  avatar: PropTypes.string,
+  message: PropTypes.string.isRequired,
+  timestamp: PropTypes.number.isRequired,
+  userId: PropTypes.string.isRequired
 }
